876059631a802f9571fdb010ee326af3da719bd1
nguyendangkin <nguyenchin0077@gmail.com>
2025-04-24 15:34:57 +0700

    Tích hợp Passport

--- 02-nest-rn-starter/src/auth/auth.controller.ts
+++ 02-nest-rn-starter/src/auth/auth.controller.ts
@@ -6,19 +6,29 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  Request,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from '@/auth/passport/local-auth.guard';
+import { JwtAuthGuard } from '@/auth/passport/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
   @Post('login')
-  login(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.signIn(
-      createAuthDto.username,
-      createAuthDto.password,
-    );
+  login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
--- 02-nest-rn-starter/src/auth/auth.module.ts
+++ 02-nest-rn-starter/src/auth/auth.module.ts
@@ -5,6 +5,9 @@ import { UsersService } from '@/modules/users/users.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from '@/modules/users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from '@/auth/passport/local.strategy';
+import { JwtStrategy } from '@/auth/passport/jwt.strategy';
 
 @Module({
   imports: [
@@ -19,8 +22,9 @@ import { UsersModule } from '@/modules/users/users.module';
       }),
       inject: [ConfigService],
     }),
+    PassportModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
