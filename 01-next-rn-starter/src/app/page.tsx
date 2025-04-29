2c225f11b8ea78ffad4f45b1ffb7e7b69213aa25
nguyendangkin <nguyenchin0077@gmail.com>
2025-04-29 15:22:19 +0700

    07:21:56 Setup Authjs (Next-auth)

--- 01-next-rn-starter/src/app/page.tsx
+++ 01-next-rn-starter/src/app/page.tsx
@@ -1,9 +1,9 @@
 import HomePage from "@/components/layout/homepage";
 
 export default function Home() {
-  return (
-    <div>
-      <HomePage />
-    </div>
-  );
+    return (
+        <div>
+            <HomePage />
+        </div>
+    );
 }
