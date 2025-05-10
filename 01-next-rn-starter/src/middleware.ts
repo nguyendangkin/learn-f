193abe877b20cd85f015f3cb7483e2cd3980ad24
nguyendangkin <nguyenchin0077@gmail.com>
2025-05-10 16:22:34 +0700

    end

--- 01-next-rn-starter/src/middleware.ts
+++
@@ -1,9 +0,0 @@
-export { auth as middleware } from "@/auth";
-
-export const config = {
-    matcher: [
-        // '/((?!auth).*)(.+)|/verify',
-        // "/((?!api|_next/static|_next/image|favicon.ico|/|/auth).*)",
-        "/((?!api|_next/static|_next/image|favicon.ico|auth|verify|$).*)",
-    ],
-};
